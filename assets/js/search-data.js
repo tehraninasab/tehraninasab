930ed42afd9f9540b97a9052857abc11f25c0bdd
tehraninasab <50144546+tehraninasab@users.noreply.github.com>
2025-04-19 19:25:58 +0000

    Deploying to gh-pages from @ tehraninasab/tehraninasab.github.io@32089207e9dadb1a6ecf2b1e1d65c896cd002834 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,35 +501,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%7A%61%68%72%61.%74%65%68%72%61%6E%69%6E%61%73%61%62@%6D%61%69%6C.%6D%63%67%69%6C%6C.%63%61", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/tehraninasab", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/zahra-tehraninasab", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=0MaybgkAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
